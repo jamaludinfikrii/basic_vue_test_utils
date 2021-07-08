b4df1d42793010f10fe09663fbd268f8d67b80f2
Fiqri <jamaludinfikrii@gmail.com>
2021-07-08 19:07:23 +0700

    mounting option

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 // import Greeting from './Greeting.vue'
 // import RandomGenerator from './RandomGenerator.vue'
 // import EmailValidation from './EmailValidation.vue'
-import Counter from './Counter.vue'
+// import Counter from './Counter.vue'
+import Mount from './Mount.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(Counter),
+  render: h => h(Mount),
 }).$mount('#app')
