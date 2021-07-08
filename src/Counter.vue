b9d8a769efdae712e98754ac5974b1e554068125
Fiqri <jamaludinfikrii@gmail.com>
2021-07-08 08:17:19 +0700

    tdd in action

--- src/Counter.vue
+++ src/Counter.vue
@@ -2,6 +2,11 @@
   <div>
     <center>
       <p>Counter</p>
+      <div>
+        <button ref="buttonMinus" @click="onMinusButtonClick"> - </button>
+        <span ref="numberRef">{{counterNumber}}</span>
+        <button ref="buttonPlus" @click="onPlusButtonClick"> + </button>
+      </div>
     </center>
   </div>
 </template>
@@ -10,6 +15,28 @@
 
 export default {
   name: 'Counter',
+  data(){
+    return{
+      counterNumber: 0
+    }
+  },
+
+  methods: {
+    onPlusButtonClick(){
+      if(this.counterNumber >= 10){
+        this.counterNumber = this.counterNumber + 10
+      }else{
+        this.counterNumber = this.counterNumber + 1
+      }
+    },
+    onMinusButtonClick(){
+      if(this.counterNumber >= 10){
+        this.counterNumber = this.counterNumber - 10
+      }else if(this.counterNumber >= 1){
+        this.counterNumber = this.counterNumber - 1
+      }
+    }
+  }
 }
 
 </script>
